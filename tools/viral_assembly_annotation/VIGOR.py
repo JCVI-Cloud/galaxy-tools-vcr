b2818d63d2a938d508d745639b4d882f8288fc99
Ubuntu <ubuntu@ip-10-70-17-196.ec2.internal>
2012-12-06 21:58:01 +0000

    Redirect output to VIGOR's default test directory

--- tools/viral_assembly_annotation/VIGOR.py
+++ tools/viral_assembly_annotation/VIGOR.py
@@ -11,8 +11,9 @@ def __main__():
 	(inputFasta,outputALN,outputCDS,outputFS,outputPEP,outputRPT,outputSTATS,outputTBL) = sys.argv[1:]
 	
 	# Set up output variables
-	outputDir = "/usr/local/tools/vigor/output"
+	outputDir = "/usr/local/tools/vigor/test"
 	outputName = "output"
+	outputLog = "%s/%s_log.txt"      % (outputDir,outputName)
 	vigorOutputALN = "%s/%s.aln"     % (outputDir,outputName)
 	vigorOutputCDS = "%s/%s.cds"     % (outputDir,outputName)
 	vigorOutputFS = "%s/%s.fs"       % (outputDir,outputName)
@@ -21,8 +22,12 @@ def __main__():
 	vigorOutputSTATS = "%s/%s.stats" % (outputDir,outputName)
 	vigorOutputTBL = "%s/%s.tbl"     % (outputDir,outputName)
 	
+	# Create empty output log file (cannot be created by VIGOR command alone).
+	os.system("sudo touch %s" % outputLog)
+	os.system("sudo chmod 777 %s" % outputLog)
+	
 	# Run command
-	command = "/usr/local/tools/vigor/prod3/VIGOR3.pl -A -i %s -O %s/%s > %s/%s_log.txt" % (inputFasta,outputDir,outputName,outputDir,outputName)
+	command = "/usr/local/tools/vigor/prod3/VIGOR3.pl -A -i %s -O %s/%s > %s" % (inputFasta,outputDir,outputName,outputLog)
 	os.system("sudo %s" % command)
 	os.system("sudo chmod 777 %s/%s.*" % (outputDir,outputName))
 	os.system("sudo cp %s %s" % (vigorOutputALN,outputALN))
