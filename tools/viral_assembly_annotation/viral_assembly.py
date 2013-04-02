7200410f81c827cecf39061d6e49daea96a8c53b
Ubuntu <ubuntu@ip-10-46-133-128.ec2.internal>
2013-04-02 20:10:13 +0000

    Uses subprocess instead of os.system. All file generation and processing now done in galaxy's current working directory, instead of /usr/local/VHTNGS/project, removing need for timestamped output directories. No need for sudo, with galaxy user now owner of files and processes done on galaxy.

--- tools/viral_assembly_annotation/viral_assembly.py
+++ tools/viral_assembly_annotation/viral_assembly.py
@@ -4,52 +4,35 @@
 Takes input from viral_assembly.xml and sends it to vir-assembly-pipeline.sh.
 """
 
-import os, sys, subprocess, tempfile, shutil, datetime
+import os, sys, subprocess, tempfile, shutil
 
 def __prepare_input__(draftFilename,finalFilename,finalDir):
 	result = "%s/%s" % (finalDir,finalFilename)
 	
 	if draftFilename == "None":
-		os.system("sudo touch %s" % result)
+		subprocess.call("rm -f %s" % result, shell=True)
+		subprocess.call("touch %s" % result, shell=True)
 	else:
-		os.system("sudo cp %s %s" % (draftFilename,result))
+		subprocess.call("cp %s %s" % (draftFilename,result), shell=True)
 	
 	return result
 
 def __main__():
-	# Parse the command line options and acquire time.
+	# Parse the command line options and get the current working directory in galaxy (location in which galaxy will work on this job)
 	(input_454,input_Sanger,input_Solexa,input_Solexa_tp,database,outputFasta) = sys.argv[1:]
-	time = datetime.datetime.now()
+	currentDir = os.getcwd()
 	
-	# Set up relevant directories for viral assembly.
-	mainDir = "/usr/local/VHTNGS"
-	outputTime = time.strftime("%Y-%m-%d_%H-%M-%S")
-	outputDirPath = "%s/project/%s" % (mainDir,outputTime)
-	outLog = "%s/project/viral_assembly_%s.log" % (mainDir,outputTime)
+	# Copy the input files to the current location, creating empty dummy files for those inputs not given by user (required for viral assembly)
+	input_454_ready = __prepare_input__(input_454,"input_454.sff",currentDir)
+	input_Sanger_ready = __prepare_input__(input_Sanger,"input_Sanger.fasta",currentDir)
+	input_Solexa_ready = __prepare_input__(input_Solexa,"input_Solexa.fastq",currentDir)
+	input_Solexa_tp_ready = __prepare_input__(input_Solexa_tp,"input_Solexa.fastq.trimpoints",currentDir)
 	
-	# Copy the input files to the VHTNGS location. If an input file was not given, create an empty dummy file as a replacement.
-	os.system("sudo rm -f %s/input_454.sff" % mainDir)
-	os.system("sudo rm -f %s/input_Sanger.fasta" % mainDir)
-	os.system("sudo rm -f %s/input_Solexa.fastq" % mainDir)
-	os.system("sudo rm -f %s/input_Solexa.fastq.trimpoints" % mainDir)
-	
-	input_454_ready = __prepare_input__(input_454,"input_454.sff",mainDir)
-	input_Sanger_ready = __prepare_input__(input_Sanger,"input_Sanger.fasta",mainDir)
-	input_Solexa_ready = __prepare_input__(input_Solexa,"input_Solexa.fastq",mainDir)
-	input_Solexa_tp_ready = __prepare_input__(input_Solexa_tp,"input_Solexa.fastq.trimpoints",mainDir)
-	
-	# Set up command and output structure.	
-	command = "%s/vir-assembly-pipeline.sh %s %s %s %s %s %s" % (mainDir,input_454_ready,input_Sanger_ready,input_Solexa_ready,input_Solexa_tp_ready,database,outputTime)
-	virAssemblyOutput = "%s/mapping/sample_hybrid_edited_refs_consensus.fasta" % outputDirPath
-	os.system("sudo touch %s" % outLog)
-	os.system("sudo chmod 777 %s" % outLog)
-	
-	# Execute command and move final output to galaxy output area.
-	outLogFH = open(outLog,"a")
-	subprocess.call("sudo %s" % command,shell=True,stdout=outLogFH,stderr=subprocess.STDOUT)
-	outLogFH.close()
-	os.system("sudo chmod 777 %s" % virAssemblyOutput)
-	os.system("sudo cp %s %s" % (virAssemblyOutput,outputFasta))
+	# Execute the viral assembly pipeline, then copy final consensus FASTA into expected galaxy history item outputFasta.
+	virAssemblyCommand = "/usr/local/VHTNGS/vir-assembly-pipeline.sh %s %s %s %s %s %s" % (input_454_ready,input_Sanger_ready,input_Solexa_ready,input_Solexa_tp_ready,database,currentDir)
+	virAssemblyOutput = "%s/mapping/sample_hybrid_edited_refs_consensus.fasta" % currentDir
+	subprocess.call(virAssemblyCommand, shell=True)
+	subprocess.call("cp %s %s" % (virAssemblyOutput,outputFasta), shell=True)
 
 if __name__=="__main__": 
   __main__()
