bb84b323a006ee5a551a1a03152ab15ee5d4b396
Ubuntu <ubuntu@ip-10-46-133-128.ec2.internal>
2013-04-02 20:58:39 +0000

    Uses subprocess instead of os.system. All file generation and processing now done in galaxy's current working directory, instead of /usr/local/VIGOR/tools/vigor/test,removing need for timestamped output directories. No need for sudo, with galaxy user now owner of files and processes done on galaxy.

--- tools/viral_assembly_annotation/VIGOR.py
+++ tools/viral_assembly_annotation/VIGOR.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python
 
 """
-Runs VIGOR3 on given input fasta file.
+Runs VIGOR3 and command-line VICVB on given input fasta file.
 """
 
 import os, sys, subprocess, tempfile, shutil
 
 def __main__():
-	# Parse the command line options
-	(inputFasta,outputALN,outputCDS,outputFS,outputPEP,outputRPT,outputSTATS,outputTBL) = sys.argv[1:]
+	# Parse the command line options and get the current working directory in galaxy (location in which galaxy will work on this job)
+	(inputFasta,outputALN,outputCDS,outputFS,outputPEP,outputRPT,outputSTATS,outputTBL,out_VCB_jbrowse,out_VCB_jbrowse_extra_files_path) = sys.argv[1:]
+	currentDir = os.getcwd()
 	
-	# Set up output variables
-	outputDir = "/usr/local/VIGOR/tools/vigor/test"
+	# Set up output variables for all outputs EXCEPT the outputs for JBrowse (last two in parsing), those two are directly sent to galaxy
 	outputName = "output"
-	outputLog = "%s/%s_log.txt"      % (outputDir,outputName)
-	vigorOutputALN = "%s/%s.aln"     % (outputDir,outputName)
-	vigorOutputCDS = "%s/%s.cds"     % (outputDir,outputName)
-	vigorOutputFS = "%s/%s.fs"       % (outputDir,outputName)
-	vigorOutputPEP = "%s/%s.pep"     % (outputDir,outputName)
-	vigorOutputRPT = "%s/%s.rpt"     % (outputDir,outputName)
-	vigorOutputSTATS = "%s/%s.stats" % (outputDir,outputName)
-	vigorOutputTBL = "%s/%s.tbl"     % (outputDir,outputName)
+	outputLog = "%s/%s_log.txt"      % (currentDir,outputName)
+	vigorOutputALN = "%s/%s.aln"     % (currentDir,outputName)
+	vigorOutputCDS = "%s/%s.cds"     % (currentDir,outputName)
+	vigorOutputFS = "%s/%s.fs"       % (currentDir,outputName)
+	vigorOutputPEP = "%s/%s.pep"     % (currentDir,outputName)
+	vigorOutputRPT = "%s/%s.rpt"     % (currentDir,outputName)
+	vigorOutputSTATS = "%s/%s.stats" % (currentDir,outputName)
+	vigorOutputTBL = "%s/%s.tbl"     % (currentDir,outputName)
 	
-	# Create empty output log file (cannot be created by VIGOR command alone).
-	os.system("sudo touch %s" % outputLog)
-	os.system("sudo chmod 777 %s" % outputLog)
+	# Run VIGOR and copy output files to corresponding galaxy outputs
+	vigorCommand = "/usr/local/VIGOR/tools/vigor/prod3/VIGOR3.pl -A -i %s -O %s/%s > %s" % (inputFasta,currentDir,outputName,outputLog)
+	subprocess.check_call((vigorCommand,), shell=True)
+	subprocess.check_call("chmod 777 %s/%s.*" % (currentDir,outputName), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputALN,outputALN), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputCDS,outputCDS), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputFS,outputFS), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputPEP,outputPEP), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputRPT,outputRPT), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputSTATS,outputSTATS), shell=True)
+	subprocess.check_call("cp %s %s" % (vigorOutputTBL,outputTBL), shell=True)
 	
-	# Run command
-	command = "/usr/local/VIGOR/tools/vigor/prod3/VIGOR3.pl -A -i %s -O %s/%s > %s" % (inputFasta,outputDir,outputName,outputLog)
-	os.system("sudo %s" % command)
-	os.system("sudo chmod 777 %s/%s.*" % (outputDir,outputName))
-	os.system("sudo cp %s %s" % (vigorOutputALN,outputALN))
-	os.system("sudo cp %s %s" % (vigorOutputCDS,outputCDS))
-	os.system("sudo cp %s %s" % (vigorOutputFS,outputFS))
-	os.system("sudo cp %s %s" % (vigorOutputPEP,outputPEP))
-	os.system("sudo cp %s %s" % (vigorOutputRPT,outputRPT))
-	os.system("sudo cp %s %s" % (vigorOutputSTATS,outputSTATS))
-	os.system("sudo cp %s %s" % (vigorOutputTBL,outputTBL))
+	# Run VICVB, submit expected galaxy output objects as the output parameters (last two in vicvb_galaxy_tool)
+	vicvbCommand = "/usr/local/VICVB/vicvb_galaxy_tool to-jbrowse None '%s' '%s' '%s' '%s'" % (inputFasta,currentDir,out_VCB_jbrowse,out_VCB_jbrowse_extra_files_path)
+	subprocess.check_call((vicvbCommand,), shell=True)
 
 if __name__=="__main__": 
 	__main__()
