d79a0c4ccfdc29af590891fff5c89c404a1f9d10
Your Name <rsanka@jcvi.org>
2013-08-12 16:32:48 -0400

    Removed Solexa Trimpoints input field (deprecated).

--- tools/viral_assembly_annotation/viral_assembly.py
+++ tools/viral_assembly_annotation/viral_assembly.py
@@ -19,17 +19,16 @@ def __prepare_input__(draftFilename,finalFilename,finalDir):
 
 def __main__():
 	# Parse the command line options and get the current working directory in galaxy (location in which galaxy will work on this job)
-	(input_454,input_Sanger,input_Solexa,input_Solexa_tp,database,outputFasta) = sys.argv[1:]
+	(input_454,input_Sanger,input_Solexa,database,outputFasta) = sys.argv[1:]
 	currentDir = os.getcwd()
 	
 	# Copy the input files to the current location, creating empty dummy files for those inputs not given by user (required for viral assembly)
 	input_454_ready = __prepare_input__(input_454,"input_454.sff",currentDir)
 	input_Sanger_ready = __prepare_input__(input_Sanger,"input_Sanger.fasta",currentDir)
 	input_Solexa_ready = __prepare_input__(input_Solexa,"input_Solexa.fastq",currentDir)
-	input_Solexa_tp_ready = __prepare_input__(input_Solexa_tp,"input_Solexa.fastq.trimpoints",currentDir)
 	
 	# Execute the viral assembly pipeline, then copy final consensus FASTA into expected galaxy history item outputFasta.
-	virAssemblyCommand = "/usr/local/VHTNGS/vir-assembly-pipeline.sh %s %s %s %s %s %s" % (input_454_ready,input_Sanger_ready,input_Solexa_ready,input_Solexa_tp_ready,database,currentDir)
+	virAssemblyCommand = "/usr/local/VHTNGS/vir-assembly-pipeline.sh %s %s %s %s %s" % (input_454_ready,input_Sanger_ready,input_Solexa_ready,database,currentDir)
 	virAssemblyOutput = "%s/mapping/sample_hybrid_edited_refs_consensus.fasta" % currentDir
 	subprocess.check_call((virAssemblyCommand,), shell=True)
 	subprocess.check_call("cp %s %s" % (virAssemblyOutput,outputFasta), shell=True)
