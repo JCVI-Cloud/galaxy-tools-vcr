8a61f1948a99cb689993380f952c53d8f731eb23
Your Name <rsanka@jcvi.org>
2014-02-24 13:12:02 -0500

    Updated to call new viral_assembly_pipeline.py and check for lack of reads data input

--- tools/viral_assembly_annotation/viral_assembly.py
+++ tools/viral_assembly_annotation/viral_assembly.py
@@ -24,7 +24,7 @@ def __main__():
 		sys.exit(1)
 	# Otherwise, execute the viral assembly pipeline, then copy final consensus FASTA into expected galaxy history item outputFasta.
 	else:
-		viral_assembly_cmd = "/usr/local/VHTNGS_new/viral_assembly_pipeline.py %s--input-Viral-DB %s --output-dir %s" % (viral_assembly_cmd_files,database,currentDir)
+		viral_assembly_cmd = "/usr/local/VHTNGS/viral_assembly_pipeline.py %s--input-Viral-DB %s --output-dir %s" % (viral_assembly_cmd_files,database,currentDir)
 		viral_assembly_fasta = "%s/final/final_consensus.fasta" % currentDir
 		subprocess.check_call((viral_assembly_cmd,), shell=True)
 		subprocess.check_call("cp %s %s" % (viral_assembly_fasta,outputFasta), shell=True)
