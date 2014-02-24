80b61341c936ebd08c9166bc111fb2a0440c72e0
Your Name <rsanka@jcvi.org>
2014-02-24 13:04:23 -0500

    Updated to call new viral_assembly_pipeline.py and check for lack of reads data input

--- tools/viral_assembly_annotation/viral_assembly.py
+++ tools/viral_assembly_annotation/viral_assembly.py
@@ -1,37 +1,33 @@
 #!/usr/bin/env python
 
 """
-Takes input from viral_assembly.xml and sends it to vir-assembly-pipeline.sh.
+Takes input from viral_assembly.xml and sends it to viral_assembly_pipeline.py.
 """
 
-import os, sys, subprocess, tempfile, shutil
-
-def __prepare_input__(draftFilename,finalFilename,finalDir):
-	result = "%s/%s" % (finalDir,finalFilename)
-	
-	if draftFilename == "None":
-		subprocess.check_call("rm -f %s" % result, shell=True)
-		subprocess.check_call("touch %s" % result, shell=True)
-	else:
-		subprocess.check_call("cp %s %s" % (draftFilename,result), shell=True)
-	
-	return result
+import os, sys, subprocess, tempfile, shutil, time
 
 def __main__():
 	# Parse the command line options and get the current working directory in galaxy (location in which galaxy will work on this job)
-	(input_454,input_Sanger,input_Solexa,database,outputFasta) = sys.argv[1:]
+	(input_Illumina,input_Iontorrent,input_Sanger_seqs,input_Sanger_qual,input_454,database,outputFasta) = sys.argv[1:]
 	currentDir = os.getcwd()
 	
-	# Copy the input files to the current location, creating empty dummy files for those inputs not given by user (required for viral assembly)
-	input_454_ready = __prepare_input__(input_454,"input_454.sff",currentDir)
-	input_Sanger_ready = __prepare_input__(input_Sanger,"input_Sanger.fasta",currentDir)
-	input_Solexa_ready = __prepare_input__(input_Solexa,"input_Solexa.fastq",currentDir)
+	viral_assembly_cmd_files = ""
+	if (input_Illumina != "None"): viral_assembly_cmd_files = "%s --input-Illumina %s " % (viral_assembly_cmd_files,input_Illumina)
+	if (input_Iontorrent != "None"): viral_assembly_cmd_files = "%s --input-Iontorrent %s " % (viral_assembly_cmd_files,input_Iontorrent)
+	if (input_Sanger_seqs != "None"): viral_assembly_cmd_files = "%s --input-Sanger-seqs %s " % (viral_assembly_cmd_files,input_Sanger_seqs)
+	if (input_Sanger_qual != "None"): viral_assembly_cmd_files = "%s --input-Sanger-qual %s " % (viral_assembly_cmd_files,input_Sanger_qual)
+	if (input_454 != "None"): viral_assembly_cmd_files = "%s --input-454 %s " % (viral_assembly_cmd_files,input_454)
 	
-	# Execute the viral assembly pipeline, then copy final consensus FASTA into expected galaxy history item outputFasta.
-	virAssemblyCommand = "/usr/local/VHTNGS/vir-assembly-pipeline.sh %s %s %s %s %s" % (input_454_ready,input_Sanger_ready,input_Solexa_ready,database,currentDir)
-	virAssemblyOutput = "%s/mapping/sample_hybrid_edited_refs_consensus.fasta" % currentDir
-	subprocess.check_call((virAssemblyCommand,), shell=True)
-	subprocess.check_call("cp %s %s" % (virAssemblyOutput,outputFasta), shell=True)
+	# If no read files provided, raise error message.
+	if (viral_assembly_cmd_files == ""):
+		sys.stderr.write("No read files (Illumina, Iontorrent, Sanger, or 454) were provided. At least one is required.")
+		sys.exit(1)
+	# Otherwise, execute the viral assembly pipeline, then copy final consensus FASTA into expected galaxy history item outputFasta.
+	else:
+		viral_assembly_cmd = "/usr/local/VHTNGS_new/viral_assembly_pipeline.py %s--input-Viral-DB %s --output-dir %s" % (viral_assembly_cmd_files,database,currentDir)
+		viral_assembly_fasta = "%s/final/final_consensus.fasta" % currentDir
+		subprocess.check_call((viral_assembly_cmd,), shell=True)
+		subprocess.check_call("cp %s %s" % (viral_assembly_fasta,outputFasta), shell=True)
 
 if __name__=="__main__": 
   __main__()
